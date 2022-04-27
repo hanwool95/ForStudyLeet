2af87905678ea25bc9ee3d108941de3ef9903d3b
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-04-27 16:53:09 +0900

    4_27 Figure out

--- April/SmallestStringWithSwaps.py
+++ April/SmallestStringWithSwaps.py
@@ -59,4 +59,145 @@ class Solution:
 
 
 
+# Second Trial
+# 연결된 점들은 서로 바꿀 수 있음. 따라서 grouping 방식으로 문제 해결 가능.
+# 어제 문제(MinCostToConnectAllPoints) 적용한 1차원 group을 사용.
 
+class Solution:
+    def findGroup(self, pairs, group, weight):
+
+        for p1, p2 in pairs:
+            p1G = self.discover(p1, group)
+            p2G = self.discover(p2, group)
+            if p1G != p2G:
+
+                if weight[p1G] >= weight[p2G]:
+
+                    for i, index in enumerate(group):
+                        if index == p2G:
+                            group[i] = p1G
+
+                    group[p2G] = p1G
+                    if weight[p1G] == weight[p2G]:
+                        weight[p1G] += 1
+                else:
+                    for i, index in enumerate(group):
+                        if index == p1G:
+                            group[i] = p2G
+
+    def discover(self, p, group):
+        if p != group[p]:
+            return self.discover(group[p], group)
+        return p
+
+    def executeSet(self, group, s):
+        group_list = set(group)
+        group_dict = {}
+
+        for cur_group in group_list:
+            group_dict[cur_group] = []
+
+        for i, index in enumerate(group):
+            group_dict[index].append(i)
+
+        for group_index, value_list in group_dict.items():
+            cur_str = ""
+            for value in value_list:
+                cur_str += s[value]
+            sorted(cur_str)
+            cur_str = ''.join(sorted(cur_str))
+
+            for i in range(len(cur_str)):
+                s = ''.join((s[:value_list[i]], cur_str[i], s[value_list[i] + 1:]))
+        return s
+
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+
+        group = [x for x in range(len(s))]
+        weight = [0 for i in range(len(s))]
+
+        self.findGroup(pairs, group, weight)
+        s = self.executeSet(group, s)
+
+        return s
+
+
+# 정답은 찾아 나감.
+# 하지만 time limit. 단순히 O(n^2)로는 통과가 안되는 것으로 보임.
+
+
+
+
+# Third Trial
+# Grouping 에서 Dictionary를 활용. 최대한 runtime 줄이는 방향
+
+class Groups:
+
+    def __init__(self, s):
+        self.group = [x for x in range(len(s))]
+        self.weight = [0 for i in range(len(s))]
+        self.Gdict = {}
+
+    def discover(self, p):
+        if p != self.group[p]:
+            return self.discover(self.group[p])
+        return self.group[p]
+
+    def grouping(self, parent, target):
+        self.group[target] = parent
+        if self.weight[parent] == 0:
+            self.Gdict[parent] = []
+
+        if self.weight[target] > 0:
+            for x in self.Gdict[target]:
+                self.Gdict[parent].append(x)
+            self.Gdict[parent].append(target)
+            del self.Gdict[target]
+        else:
+            self.Gdict[parent].append(target)
+
+    def findGroup(self, pairs):
+        for p1, p2 in pairs:
+            p1G = self.discover(p1)
+            p2G = self.discover(p2)
+            if p1G != p2G:
+
+                if self.weight[p1G] >= self.weight[p2G]:
+
+                    self.grouping(p1G, p2G)
+
+                    if self.weight[p1G] == self.weight[p2G]:
+                        self.weight[p1G] += 1
+
+
+                else:
+                    self.grouping(p2G, p1G)
+
+
+class Solution:
+    def executeSet(self, group_dict, s):
+
+        for group_index, value_list in group_dict.items():
+            cur_str = ""
+            value_list.append(group_index)
+            value_list.sort()
+            for value in value_list:
+                cur_str += s[value]
+            sorted(cur_str)
+            cur_str = ''.join(sorted(cur_str))
+
+            for i in range(len(cur_str)):
+                s = ''.join((s[:value_list[i]], cur_str[i], s[value_list[i] + 1:]))
+        return s
+
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+
+        Group = Groups(s)
+
+        Group.findGroup(pairs)
+        s = self.executeSet(Group.Gdict, s)
+
+        return s
+
+Runtime: 3292 ms, faster than 5.06% of Python3 online submissions for Smallest String With Swaps.
+Memory Usage: 50.5 MB, less than 43.32% of Python3 online submissions for Smallest String With Swaps.
